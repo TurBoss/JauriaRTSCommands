92775f894f43bc5a7cb5330b6aa2df8e961a6fe0
TurBoss <j.l.toledano.l@gmail.com>
2015-02-07 03:16:24 +0100

    grabinput widget

--- LuaUI/Configs/chilitip_conf.lua
+++ LuaUI/Configs/chilitip_conf.lua
@@ -1,13 +1,6 @@
 local ignoreweapon =
 {
-	armaak = {1},
-	armcarry = {1},
-	armaas = {1},
-	armraz = {2},
-	
-	cormak = {1,3},
-	coraak = {1},
-	coramph = {2},	
+	--armaak = {1},
 }
 local iconFormat = '.png'
 
