d4bfba1e8ccf813e8ddc32f7bf66b1ad040ff69f
PerroTron <perrotronn@gmail.com>
2015-02-17 21:13:43 +0100

    colvol

--- Objects3D/erzeus.dae.lua
+++ Objects3D/erzeus.dae.lua
@@ -1,7 +1,7 @@
 model = {
-   radius = 14,
+   radius = 6,
    --height = 10,
-   midpos = {0,14,0}, -- model center offset
+   midpos = {0,18,0}, -- model center offset
    --[[tex1 = "corraid1_512.dds", -- same as S3O texture 1
    tex2 = "corraid2_512.dds", -- same as S3O texture 2
    invertteamcolor = true, -- invert tex1 alpha channel
