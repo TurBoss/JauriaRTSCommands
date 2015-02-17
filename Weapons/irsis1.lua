2e5f4bca869ab78bdd286122b8a1d80939861219
TurBoss <j.l.toledano.l@gmail.com>
2015-02-17 04:40:25 +0100

    Irsis

--- Weapons/irsis1.lua
+++ Weapons/irsis1.lua
@@ -1,4 +1,4 @@
-local weaponName="irsis2"
+local weaponName="irsis1"
 
 local weaponDef={
 	name="Ball",
@@ -9,8 +9,8 @@ local weaponDef={
 	
 	InterceptedByShieldType= 4,
 	
-	paralyzer = true,
-	paralyzeTime = 2,
+	--paralyzer = true,
+	--paralyzeTime = 2,
 	
 	--Physic/flight path
 	range=800,
@@ -61,8 +61,8 @@ local weaponDef={
 
 	--sound
 	
-	soundStart		=	"kortus2",
-	soundHit		=	"kortus3",
+	--soundStart		=	"kortus2",
+	--soundHit		=	"kortus3",
 }
 
 return lowerkeys ({[weaponName]=weaponDef})
